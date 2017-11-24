1f48423ba0f3600af2e09cddb41ab0fd5da791bf
Drew Owen <dcowen91@gmail.com>
2017-11-23 22:00:42 -0800

    finish basic flow

--- app.ts
+++ app.ts
@@ -22,17 +22,25 @@ const options = {
   transform: body => cheerio.load(body)
 };
 
+function transformResults(value: string): number[] {
+  //handle result for match against self
+  if (value === "\\u2014") {
+    return null;
+  }
+  //split on unicode em dash and convert to number
+  return value.split("\\u2013").map(Number);
+}
+
 //TODO add yargs for league / year
 
 rp(options)
   .then($ => {
-    const rowCount = $("tr").length - 1;
-    const results = new Array(rowCount);
-    const teams = new Array(rowCount);
+    const results = new Array();
+    const teams = new Array();
     $("tr").each((i: number, tr: CheerioElement) => {
+      const $$ = cheerio.load(tr);
       if (i === 0) {
         //header row;
-        const $$ = cheerio.load(tr);
         $$("a").each((j: number, a: CheerioElement) => {
           const name = a.attribs["href"];
           const fullName = name
@@ -46,14 +54,29 @@ rp(options)
           teams.push(team);
         });
       } else {
-        const $$ = cheerio.load(tr);
+        const currentRow = new Array();
         $$("td").each((j: number, td: CheerioElement) => {
-          let result = null;
-          console.log(td.lastChild);
-          // if (td.lastChild != null) {
-          // }
+          const lastChild = td.lastChild;
+          if (!!lastChild.lastChild) {
+            // Handle cells with links for rivalries
+            currentRow.push(transformResults(lastChild.lastChild.nodeValue));
+          } else if (!!lastChild.nodeValue) {
+            currentRow.push(transformResults(lastChild.nodeValue));
+          }
         });
+        results.push(currentRow);
       }
     });
+    const output = { teams, results };
+    const fileName = `results_${league.substr(0, 4)}_${season.substr(
+      0,
+      4
+    )}.json`;
+    fs
+      .writeJSON(fileName, output)
+      .then(
+        () => console.log(`SUCCESS: wrote ${fileName}`),
+        err => console.log("ERROR" + err)
+      );
   })
   .catch(err => console.log(err));
